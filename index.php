e67cfd42f849f72451f6a11f79929e5633018152
LunaTarea <2524160034@e.uttecamac.edu.mx>
2026-03-29 15:46:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-// Hardcoded secret
+// Ya no hardcoded
 $password = getenv("PASSWORD"); 
 
-// SQL Injection
-$id = $_GET['id'];
-$query = "SELECT * FROM users WHERE id = " . $id;
+// Evitar SQL Injection (validando el dato)
+$id = intval($_GET['id']); 
+$query = "SELECT * FROM users WHERE id = $id";
 
-// Uso inseguro
-eval($_GET['code']);
 ?>
